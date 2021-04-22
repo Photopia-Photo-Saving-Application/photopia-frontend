1a1ec8b0e9eeb9d3ede2e18dce8add173be0718f
sumaiyaazad <azadsumaiya00@gmail.com>
2021-04-22 19:20:29 +0600

    initial commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: colors.cyan.lighten3,
+                secondary: '#b0bec5',
+                accent: '#8c9eff',
+                error: colors.red.accent3,
+            },
+        },
+    },
+});
