a20b8f6680fa7ce0d2e9eb48756abe6a393ebc59
sumaiyaazad <azadsumaiya00@gmail.com>
2021-04-22 23:14:01 +0600

    signupverification design done

---
+++ src/views/SignUpVerification.vue
@@ -0,0 +1,22 @@
+<template>
+<UserForm>
+    <template #messageField>
+        <p style="font-family: 'New Tegomin', serif;">
+        Verification Complete !! <br>
+            Please <router-link to="/signIn" style="text-decoration: none">Sign In</router-link>
+        </p>
+    </template>
+</UserForm>
+</template>
+
+<script>
+import UserForm from "@/components/UserForm/UserForm";
+export default {
+name: "SignUpVerification",
+    components: {UserForm}
+}
+</script>
+
+<style scoped>
+
+</style>
