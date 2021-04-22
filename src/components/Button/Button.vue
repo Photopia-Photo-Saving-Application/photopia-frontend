8423ec1ccfdd150ee9eaa5d245b93cb0784103e4
sumaiyaazad <azadsumaiya00@gmail.com>
2021-04-22 20:28:34 +0600

    added button component

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-btn raised class="mt-5" :color=color :style="{width: width}">
+        {{text}}
+    </v-btn>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        'color': {
+            required: true
+        },
+        'text': {
+            required: true
+        },
+        'width':{
+            required:true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
