b9d8b344ec3fd74899573cee98082ff93304db6c
sumaiyaazad <azadsumaiya00@gmail.com>
2021-04-22 20:29:03 +0600

    added userform component

---
+++ src/components/UserForm/UserForm.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-container fluid style="margin-top: 10%;background-image: url('src/assets/image/background.jpg'); background-size: cover;">
+        <v-row>
+            <v-col cols="12" lg="4" sm="2"></v-col>
+            <v-col cols="12" lg="4" sm="8">
+
+                <v-card elevation="5" class="pa-16">
+                    <v-card-title class=" justify-center" style="font-family: 'Dancing Script', cursive; font-size: 2em"> PHOTOPIA</v-card-title>
+                    <v-card-actions >
+                        <v-form style="width: 100%;">
+                            <slot name="inputField"/>
+                            <slot name="buttonField" />
+                        </v-form>
+                    </v-card-actions>
+                    <v-card-text>
+                        <slot name="redirectionField"/>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+
+            <v-col cols="12" lg="4" sm="2">
+
+            </v-col>
+        </v-row>
+
+    </v-container>
+</template>
+
+<script>
+export default {
+name: "UserForm"
+}
+</script>
+
+<style scoped>
+
+</style>
