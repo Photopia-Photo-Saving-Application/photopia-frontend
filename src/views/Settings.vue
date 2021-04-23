91164ce0d223448434b38d7a962f44f0fef375ff
sumaiyaazad <azadsumaiya00@gmail.com>
2021-04-23 14:30:35 +0600

    settings validation don

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -7,32 +7,32 @@
             </template>
             <template #inputField>
                 <v-text-field
-                    v-model="oldpassword"
-                    :error-messages="emailErrors"
+                    type="password"
+                    v-model="currentPassword"
+                    :error-messages="currentPasswordErrors"
                     label="Enter current password"
                     required
-                    @input="$v.email.$touch()"
-                    @blur="$v.email.$touch()"
+                    @blur="$v.currentPassword.$touch()"
                 ></v-text-field>
                 <v-text-field
-                    v-model="newpassword"
-                    :error-messages="emailErrors"
+                    type="password"
+                    v-model="newPassword"
+                    :error-messages="newPasswordErrors"
                     label="Enter new password"
                     required
-                    @input="$v.email.$touch()"
-                    @blur="$v.email.$touch()"
+                    @blur="$v.newPassword.$touch()"
                 ></v-text-field>
                 <v-text-field
+                    type="password"
                     v-model="confirmPassword"
-                    :error-messages="emailErrors"
+                    :error-messages="confirmPasswordErrors"
                     label="Confirm new password"
                     required
-                    @input="$v.email.$touch()"
-                    @blur="$v.email.$touch()"
+                    @blur="$v.confirmPassword.$touch()"
                 ></v-text-field>
             </template>
             <template #buttonField>
-                <Button color="primary" text="CONFIRM" width="100%" :click=submit></Button>
+                <Button color="primary" text="CONFIRM" width="100%" :click=submit :disabled="$v.$anyError"></Button>
             </template>
             <template #redirectionField>
                 <UserRouterLink to1="/signIn" to2="/signIn" text1="Logout from all devices" text2="Delete account"></UserRouterLink>
@@ -47,9 +47,10 @@ import UserRouterLink from "@/components/UserRouterLink/UserRouterLink";
 import UserForm from "@/components/UserForm/UserForm";
 import MyText from "@/components/MyText/MyText";
 import {validationMixin} from "vuelidate";
-import {email, maxLength, required} from "vuelidate/lib/validators";
+import {email, helpers, maxLength, minLength, required, sameAs} from "vuelidate/lib/validators";
 import Button from "@/components/Button/Button";
 import UserRouterLink1 from "@/components/UserRouterLink/UserRouterLink1";
+const passwordValidator = helpers.regex('alphaNumAndDot',/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{5,30}$/);
 
 export default {
     name: "Settings",
@@ -57,51 +58,54 @@ export default {
     mixins: [validationMixin],
 
     validations: {
-        name: {required, maxLength: maxLength(10)},
-        email: {required, email},
-        select: {required},
-        checkbox: {
-            checked(val) {
-                return val
-            },
+        currentPassword:{
+            required,
+            minLength:minLength(5),
+            maxLength:maxLength(30),
+            passwordValidator
+        },
+        newPassword:{
+            required,
+            minLength:minLength(5),
+            maxLength:maxLength(30),
+            passwordValidator
+        },
+        confirmPassword:{
+            sameAs:sameAs('newPassword')
         },
     },
 
     data: () => ({
-        name: '',
-        email: '',
-        oldpassword:'',
-        newpassword:'',
+        currentPassword:'',
+        newPassword:'',
         confirmPassword:''
     }),
 
     computed: {
-        checkboxErrors() {
+        currentPasswordErrors() {
             const errors = []
-            if (!this.$v.checkbox.$dirty) return errors
-            !this.$v.checkbox.checked && errors.push('You must agree to continue!')
+            if (!this.$v.currentPassword.$dirty) return errors
+            !this.$v.currentPassword.minLength && errors.push('Password must be at least 5 characters long')
+            !this.$v.currentPassword.maxLength && errors.push('Password must be at most 30 characters long')
+            !this.$v.currentPassword.passwordValidator && errors.push('Password must contain at least a digit and special character')
+            !this.$v.currentPassword.required && errors.push('Password is required.')
             return errors
         },
-        selectErrors() {
+        newPasswordErrors() {
             const errors = []
-            if (!this.$v.select.$dirty) return errors
-            !this.$v.select.required && errors.push('Item is required')
+            if (!this.$v.newPassword.$dirty) return errors
+            !this.$v.newPassword.minLength && errors.push('Password must be at least 5 characters long')
+            !this.$v.newPassword.maxLength && errors.push('Password must be at most 30 characters long')
+            !this.$v.newPassword.passwordValidator && errors.push('Password must contain at least a digit and special character')
+            !this.$v.newPassword.required && errors.push('Password is required.')
             return errors
         },
-        nameErrors() {
+        confirmPasswordErrors() {
             const errors = []
-            if (!this.$v.name.$dirty) return errors
-            !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-            !this.$v.name.required && errors.push('Name is required.')
+            if (!this.$v.confirmPassword.$dirty) return errors
+            !this.$v.confirmPassword.sameAs && errors.push('Password does not match')
             return errors
-        },
-        emailErrors() {
-            const errors = []
-            if (!this.$v.email.$dirty) return errors
-            !this.$v.email.email && errors.push('Must be valid e-mail')
-            !this.$v.email.required && errors.push('E-mail is required')
-            return errors
-        },
+        }
     },
 
     methods: {
