43264ad670b2af0a9c9bd360ef20c6f4c0aeb910
sumaiyaazad <azadsumaiya00@gmail.com>
2021-04-25 22:46:53 +0600

    added button for image upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,38 +36,12 @@
           </v-img>
       </v-col>
       </v-row>
-          <v-dialog
-              v-model="dialog"
-              persistent
-              max-width="290"
-          >
-              <template v-slot:activator="{ on, attrs }">
-
-              </template>
-              <v-card>
-                  <v-card-title class="headline">
-                      Use Google's location service?
-                  </v-card-title>
-                  <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
-                  <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn
-                          color="green darken-1"
-                          text
-                          @click="dialog = false"
-                      >
-                          Disagree
-                      </v-btn>
-                      <v-btn
-                          color="green darken-1"
-                          text
-                          @click="dialog = false"
-                      >
-                          Agree
-                      </v-btn>
-                  </v-card-actions>
-              </v-card>
-          </v-dialog>
+          <v-fab-transition>
+              <v-btn color="primary" dark fixed bottom right fab  v-bind="attrs"
+                     v-on="on">
+                  <v-icon>mdi-plus</v-icon>
+              </v-btn>
+          </v-fab-transition>
       </v-container>
 
   </div>
